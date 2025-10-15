6eee9df12e2df013f5c8b77a2e2eed59b1fb7450
Harsh Patel <harshp.avidclan@gmail.com>
2025-10-15 13:30:05 +0530

    url roting check

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 5662,
   },
-  base: '/time_card/',
+  base: '/time_card',
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
